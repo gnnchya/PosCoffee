014c7d61e5abbe22a98c1cb7c3750c6bea26c223
pondnarawich <pondnarawich@gmail.com>
2021-06-21 15:05:37 +0700

    -

--- cart/repository/mongodb/search.go
+++ cart/repository/mongodb/search.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	domain "github.com/gnnchya/PosCoffee/cart/domain"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	//"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	//"strconv"
@@ -41,46 +42,19 @@ func toString(resultArray []domain.InsertQ, err error) (string, error){
 	}
 	return result, err
 }
-//
-//func (repo *Repository)Search(ctx context.Context,search *domain.SearchValue) /*(result []domain.InsertQ,err error)*/ (result string, err error){
-//	fmt.Println("Searching for ",search.Value,"in",search.Type)
-//	switch search.Type{
-//	case "name", "actual_name", "gender", "super_power":
-//		cursor, err := repo.Coll.Find(ctx, bson.M{search.Type: primitive.Regex{Pattern: search.Value, Options: "i"}})
-//		if err != nil {
-//			return toString(AddToArray(cursor,err,ctx))
-//		}
-//		return toString(AddToArray(cursor,err,ctx))
-//	case "alive":
-//		alive, err := strconv.ParseBool(search.Value)
-//		if err != nil {
-//			return "Can not convert "+search.Value+" to boolean", err
-//		}
-//		cursor, err := repo.Coll.Find(ctx, bson.M{search.Type: alive})
-//		if err != nil {
-//			return toString(AddToArray(cursor,err,ctx))
-//		}
-//		return toString(AddToArray(cursor,err,ctx))
-//	case "height":
-//		cursor, err := repo.Coll.Find(ctx, bson.M{"$where":
-//			"/"+search.Value+".*/.test(this."+search.Type+")"})
-//		if err != nil {
-//			return toString(AddToArray(cursor,err,ctx))
-//		}
-//		return toString(AddToArray(cursor,err,ctx))
-//	case "both_name":
-//		cursor, err := repo.Coll.Find(ctx,
-//			bson.M{
-//				"$or": bson.A{
-//					bson.M{"name": primitive.Regex{Pattern: search.Value, Options: "i"}},
-//					bson.M{"actual_name": primitive.Regex{Pattern: search.Value, Options: "i"}},
-//				}})
-//		if err != nil {
-//			return toString(AddToArray(cursor,err,ctx))
-//		}
-//		return toString(AddToArray(cursor,err,ctx))
-//	default:
-//		return "No "+search.Type+" exist", err
-//	}
-//}
-//
+
+func (repo *Repository)Search(ctx context.Context,search *domain.SearchValue) /*(result []domain.InsertQ,err error)*/ (result string, err error){
+	fmt.Println("Searching for ",search.Value,"in")
+	cursor, err := repo.Coll.Find(ctx,
+		bson.M{
+			"$or": bson.A{
+				bson.M{"name": primitive.Regex{Pattern: search.Value, Options: "i"}},
+				bson.M{"ingredient": primitive.Regex{Pattern: search.Value, Options: "i"}},
+				bson.M{"category": primitive.Regex{Pattern: search.Value, Options: "i"}},
+			}})
+	if err != nil {
+		return toString(AddToArray(cursor,err,ctx))
+	}
+	return toString(AddToArray(cursor,err,ctx))
+}
+
