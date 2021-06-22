8c71f2a0c06f41f0139765ffeb722d57ff27af04
pondnarawich <pondnarawich@gmail.com>
2021-06-22 16:23:04 +0700

    -

--- cart/service/user/userin/create.go
+++ cart/service/user/userin/create.go
@@ -4,45 +4,19 @@ import (
 	"github.com/gnnchya/PosCoffee/cart/domain"
 )
 
-//type Cart struct{
-//	Menu		Menu `bson:"menu" json:"menu"`
-//	Amount 		int64   `bson:"amount" json:"amount"`
-//	Option 		string   `bson:"option" json:"option"`
-//}
-//
-//type Menu struct{
-//	ID         		string   `bson:"_id" json:"id"`
-//	Category       	string   `bson:"category" json:"category"`
-//	Name 			string   `bson:"name" json:"name" validate:"required"`
-//	Ingredient 		[]string `bson:"ingredient" json:"ingredient"`
-//	Price      		int64    `bson:"price" json:"price"`
-//	Available 		bool	 `bson:"available" json:"available"`
-//	Code int `json:"code"`
-//	Err error `json:"err"`
-//}
-
 type CreateInput struct {
-	ID         		string   `bson:"_id" json:"id"`
-	CustomerID 		string   `bson:"customer_id" json:"customer_id"`
-	Cart			[]domain.Cart		`bson:"cart" json:"cart"`
-	Purchase     		bool   `bson:"status" json:"status"`
-	Price  			int64    `bson:"price" json:"price"`
-	TypeOfOrder 	string `bson:"type_of_order" json:"type_of_order"`
-	Destination    	string     `bson:"destination" json:"destination"`
-	Time			int64 	`bson:"time" json:"time"`
-	Code int `json:"code"`
-	Err error `json:"err"`
+	ID 				string  `bson:"_id" json:"_id"`
+	CustomerID 		string  `bson:"customer_id" json:"customer_id"`
+	Menu			[]domain.Menu 	`bson:"menu" json:"menu"`
+	Code 			int 	`json:"code"`
+	Err 			error	`json:"err"`
 }
 
 func (input *CreateInput)CreateInputToUserDomain() (user *domain.CreateStruct) {
 	return &domain.CreateStruct{
 		ID:             input.ID,
 		CustomerID: input.CustomerID,
-		Cart: input.Cart,
-		Purchase: input.Purchase,
-		Price: input.Price,
-		TypeOfOrder: input.TypeOfOrder,
-		Destination: input.Destination,
+		Menu: input.Menu,
 		Code: input.Code,
 		Err: input.Err,
 	}
--- cart/service/user/userin/update.go
+++ cart/service/user/userin/update.go
@@ -6,40 +6,18 @@ import (
 )
 
 type UpdateInput struct {
-	ID         		string   `bson:"_id" json:"id"`
-	CustomerID 		string   `bson:"customer_id" json:"customer_id"`
-	Cart  			[]struct{
-		Menu	struct{
-			ID         		string   `bson:"_id" json:"id"`
-			Category       	string   `bson:"category" json:"category"`
-			Name 			string   `bson:"name" json:"name" validate:"required"`
-			Ingredient 		[]string `bson:"ingredient" json:"ingredient"`
-			Price      		int64    `bson:"price" json:"price"`
-			Available 		bool	 `bson:"available" json:"available"`
-			Code int `json:"code"`
-			Err error `json:"err"`
-		}	`bson:"menu" json:"menu"`
-		Amount 		int64   `bson:"amount" json:"amount"`
-		Option 		string   `bson:"option" json:"option"`
-	}   `bson:"cart" json:"cart"`
-	Purchase     		bool   `bson:"status" json:"status"`
-	Price  			int64    `bson:"price" json:"price"`
-	TypeOfOrder 	string `bson:"type_of_order" json:"type_of_order"`
-	Destination    	string     `bson:"destination" json:"destination"`
-	Time			int64 	`bson:"time" json:"time"`
-	Code int `json:"code"`
-	Err error `json:"err"`
+	ID 				string  `bson:"_id" json:"_id"`
+	CustomerID 		string  `bson:"customer_id" json:"customer_id"`
+	Menu			[]domain.Menu 	`bson:"menu" json:"menu"`
+	Code 			int 	`json:"code"`
+	Err 			error	`json:"err"`
 }
 
 func UpdateInputToUserDomain(input *UpdateInput) (user *domain.UpdateStruct) {
 	return &domain.UpdateStruct{
 		ID:             input.ID,
 		CustomerID: input.CustomerID,
-		Cart: input.Cart,
-		Purchase: input.Purchase,
-		Price: input.Price,
-		TypeOfOrder: input.TypeOfOrder,
-		Destination: input.Destination,
+		Menu: input.Menu,
 		Code: input.Code,
 		Err: input.Err,
 	}
@@ -49,11 +27,7 @@ func (input *UpdateInput)UpdateInputToUserDomain() (user *domain.UpdateStruct) {
 	return &domain.UpdateStruct{
 		ID:             input.ID,
 		CustomerID: input.CustomerID,
-		Cart: input.Cart,
-		Purchase: input.Purchase,
-		Price: input.Price,
-		TypeOfOrder: input.TypeOfOrder,
-		Destination: input.Destination,
+		Menu: input.Menu,
 		Code: input.Code,
 		Err: input.Err,
 	}
