2b5a09a4c9ae97c181ebe0c8c42ec7aa424b2695
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-06-22 13:14:01 +0700

    ท

--- product/domain/money.go
+++ product/domain/money.go
@@ -1,33 +1,31 @@
 package domain
 
 type CreateMoneyStruct struct {
-	Value   		int64   		`bson:"value" json:"value"`
-	Amount 			int64  			`bson:"amount" json:"amount"`
-	Currency		string   		`bson:"currency" json:"currency"`
-	Code 			int 			`json:"code"`
-	Err 			error 			`json:"err"`
+	ID 			string		`bson:"-id" json:"_id"`
+	Value   	int64   	`bson:"value" json:"value"`
+	Amount 		int64  		`bson:"amount" json:"amount"`
+	Currency	string   	`bson:"currency" json:"currency"`
+	Code 		int 		`json:"code"`
+	Err 		error 		`json:"err"`
 }
 
 type DeleteMoneyStruct struct {
+	ID 		string	`bson:"-id" json:"_id"`
 	Value   int64   `bson:"value" json:"value"`
 	Code 	int 	`json:"code"`
 	Err 	error 	`json:"err"`
 }
 
 type UpdateMoneyStruct struct {
-	Value   		int64   		`bson:"value" json:"value"`
-	Amount 			int64  			`bson:"amount" json:"amount"`
-	Code 			int 			`json:"code"`
-	Err 			error 			`json:"err"`
+	ID 		string	`bson:"-id" json:"_id"`
+	Value   int64   `bson:"value" json:"value"`
+	Amount 	int64  	`bson:"amount" json:"amount"`
+	Code 	int 	`json:"code"`
+	Err 	error 	`json:"err"`
 }
 
 
 type ReadMoneyStruct struct {
-	Value   int64   `bson:"value" json:"value"`
-
+	ID 		string	`bson:"-id" json:"_id"`
 }
 
-type ReadMoneyByPageStruct struct {
-	PerPage int
-	Page    int
-}
