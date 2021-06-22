0c10cfdf15ad2e9ff6c510d4d47a3f7e2ffa51c7
proudkittapa <proudy2013@gmail.com>
2021-06-22 11:00:37 +0700

    -

--- stock/service/user/implement/read.go
+++ stock/service/user/implement/read.go
@@ -7,16 +7,16 @@ import (
 	"github.com/gnnchya/PosCoffee/stock/service/user/userin"
 )
 
-func (impl *implementation) Read(ctx context.Context, input *userin.ReadInput) ([]domain.CreateStruct, error) {
-	err := impl.validator.Validate(input)
+func (impl *implementation) Read(ctx context.Context, input *userin.ReadInput) (a domain.CreateStruct, err error) {
+	err = impl.validator.Validate(input)
 	if err != nil {
 		fmt.Println("validate", err)
-		return nil, err
+		return a, err
 	}
 	user := userin.ViewInputToUserDomain(input)
 	fmt.Println("user input view: ", user)
 	fmt.Println("user.ID: ", user.ID)
-	a, err := impl.elasRepo.View(user.ID, ctx)
+	a, err = impl.repo.Read(ctx, user.ID)
 	fmt.Println("err:", err)
 	if err != nil {
 		return a, err
--- stock/service/user/implement/search.go
+++ stock/service/user/implement/search.go
@@ -8,15 +8,15 @@ import (
 	"github.com/gnnchya/PosCoffee/stock/service/user/userin"
 )
 
-func (impl *implementation) Search(ctx context.Context, input *userin.Search) ([]domain.CreateStruct, error) {
-	err := impl.validator.Validate(input)
+func (impl *implementation) Search(ctx context.Context, input *userin.Search) (a []domain.CreateStruct, err error) {
+	err = impl.validator.Validate(input)
 	if err != nil {
 		fmt.Println("validate", err)
 		return nil, err
 	}
 	user := userin.SearchInputToUserDomain(input)
 	fmt.Println("user input search:", user)
-	a, err := impl.elasRepo.Search(user.Value, ctx)
+	//a, err := impl.repo.Search(user.Value, ctx)
 	fmt.Println("output search:", user)
 	if err != nil {
 		return a, err
