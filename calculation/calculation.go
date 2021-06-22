235f89b7bf057536c7492e8fd058c0eef702b567
pondnarawich <pondnarawich@gmail.com>
2021-06-22 15:32:42 +0700

    -

--- calculation/calculation.go
+++ calculation/calculation.go
@@ -2,19 +2,32 @@ package main
 
 import "fmt"
 
-func calculation(value int, note []int) map[int]int{
-	change := make(map[int]int)
-	var remaining = value
+type Money struct {
+	//ID 			string
+	Value   	int64
+	Amount 		int64
+	//Currency	string
+}
+
+
+func Calculation(value int64, note []Money) map[int64]int64{
+	change := make(map[int64]int64)
 	for _,i := range note{
-		if remaining >= i{
-			change[i] = remaining/i
-			remaining = value % i
+		if value >= i.Value{
+			if value/i.Value > i.Amount{
+				change[i.Value] = i.Amount
+				value = value - (i.Amount*i.Value)
+			} else {
+				change[i.Value] = value/i.Value
+				value = value % i.Value
+			}
 		}
 	}
 	return change
 }
 
 func main(){
-	thai := []int{1000,500,100,50,20,10,5,2,1}
-	fmt.Println(calculation(23451,thai))
+	thai := []Money{{1000,20},{500,2},{100,2},{50,2},
+		{20,2},{10,2},{5,2},{2,2},{1,200000}}
+	fmt.Println(Calculation(23451,thai))
 }
