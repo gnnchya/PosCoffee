c0cc315cbac2de7207aa47b41cd2764a1dd4443d
pondnarawich <pondnarawich@gmail.com>
2021-06-23 15:09:07 +0700

    -

--- cart/repository/mongodb/query.go
+++ cart/repository/mongodb/query.go
@@ -2,6 +2,7 @@ package mongodb
 
 import (
 	"context"
+	"fmt"
 	"github.com/gnnchya/PosCoffee/cart/domain"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -22,6 +23,7 @@ func (repo *Repository) Delete(ctx context.Context, id string) (err error) {
 }
 
 func (repo *Repository) Update(ctx context.Context, figure interface{}, id string) (err error) {
+	fmt.Println(figure)
 	_, err = repo.Coll.UpdateOne(ctx, bson.M{"_id": id}, bson.D{{"$set", figure}})
 	return err
 }
@@ -35,6 +37,7 @@ func (repo *Repository) Read(ctx context.Context, id string) (resultStruct domai
 	err = repo.Coll.FindOne(ctx, bson.D{{"_id", id}}).Decode(&resultBson)
 	bsonBytes, _ := bson.Marshal(resultBson)
 	_ = bson.Unmarshal(bsonBytes, &resultStruct)
+	fmt.Println(resultStruct)
 	return resultStruct, err
 }
 
