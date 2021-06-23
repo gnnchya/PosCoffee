21a5e272ad709a52d8df5556a5c287fcca18a021
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-06-23 10:19:15 +0700

    update ElasDB in menu app

--- menu/config/config.go
+++ menu/config/config.go
@@ -7,11 +7,6 @@ import (
 type Config struct {
 	AppName string `env:"APP_NAME" envDefault:"gogo_blueprint"`
 
-	// MongoDB config
-	MongoDBEndpoint         string `env:"MONGODB_ENDPOINT" envDefault:"mongodb://touch:touchja@localhost:27017"`
-	MongoDBName             string `env:"MONGODB_NAME" envDefault:"superhero"`
-	MongoDBHeroTableName string `env:"MONGODB_HERO_TABLE_NAME" envDefault:"lists"`
-
 	// Jaeger config
 	JaegerAgentHost string `env:"JAEGER_AGENT_HOST" envDefault:"localhost"`
 	JaegerAgentPort string `env:"JAEGER_AGENT_PORT" envDefault:"6831"`
