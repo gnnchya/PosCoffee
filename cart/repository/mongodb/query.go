8359b6bee84677638004a03d330bcb9b95a5fa6d
pondnarawich <pondnarawich@gmail.com>
2021-06-24 13:57:41 +0700

    -

--- cart/repository/mongodb/query.go
+++ cart/repository/mongodb/query.go
@@ -2,9 +2,9 @@ package mongodb
 
 import (
 	"context"
+	"fmt"
 	"github.com/gnnchya/PosCoffee/cart/domain"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func (repo *Repository) Create(ctx context.Context, figure interface{}) (err error) {
@@ -22,6 +22,12 @@ func (repo *Repository) Delete(ctx context.Context, id string) (err error) {
 }
 
 func (repo *Repository) Update(ctx context.Context, figure interface{}, id string) (err error) {
+	state, err := repo.CheckExistID(ctx, id)
+	if err != nil{
+		return err
+	} else if state == false{
+		return fmt.Errorf("this ID does not exist")
+	}
 	_, err = repo.Coll.UpdateOne(ctx, bson.M{"_id": id}, bson.D{{"$set", figure}})
 	return err
 }
@@ -35,13 +41,13 @@ func (repo *Repository) Read(ctx context.Context, id string) (resultStruct domai
 	return resultStruct, err
 }
 
-func (repo *Repository) ReadAll(ctx context.Context, perPage int, page int) ([]domain.CreateStruct, error) {
-	skip := int64(page * perPage)
-	limit := int64(perPage)
-	opts := options.FindOptions{
-		Skip:  &skip,
-		Limit: &limit,
-	}
-	cursor, err := repo.Coll.Find(nil, bson.M{}, &opts)
-	return AddToArray(cursor, err, ctx)
-}
+//func (repo *Repository) ReadAll(ctx context.Context, perPage int, page int) ([]domain.CreateStruct, error) {
+//	skip := int64(page * perPage)
+//	limit := int64(perPage)
+//	opts := options.FindOptions{
+//		Skip:  &skip,
+//		Limit: &limit,
+//	}
+//	cursor, err := repo.Coll.Find(nil, bson.M{}, &opts)
+//	return AddToArray(cursor, err, ctx)
+//}
