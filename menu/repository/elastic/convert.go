323be2ebd34b2c2e2a02001b6f9aeec5c194ad70
pondnarawich <pondnarawich@gmail.com>
2021-06-24 15:41:23 +0700

    -

--- menu/repository/elastic/convert.go
+++ menu/repository/elastic/convert.go
@@ -14,7 +14,10 @@ func InToStruct(r map[string]interface{}) []domain.CreateStruct{
 		temp.ID = fmt.Sprintf("%v", s.(map[string]interface{})["_id"])
 		temp.Name = fmt.Sprintf("%v", s.(map[string]interface{})["name"])
 		temp.Category= strings.Split(fmt.Sprintf("%v", s.(map[string]interface{})["category"]),",")
-		temp.Ingredient = strings.Split(fmt.Sprintf("%v", s.(map[string]interface{})["ingredient"]),",")
+		for i, in := range s.(map[string]interface{})["ingredient"].([]interface{}){
+			temp.Ingredient[i].IngredientName = fmt.Sprintf("%v", in.(map[string]interface{})["ingredient_name"])
+			temp.Ingredient[i].Amount = int64(in.(map[string]interface{})["amount"].(float64))
+		}
 		temp.Price = int64(s.(map[string]interface{})["price"].(float64))
 		temp.Available,_ = strconv.ParseBool(fmt.Sprintf("%v", s.(map[string]interface{})["available"]))
 		result = append(result, temp)
