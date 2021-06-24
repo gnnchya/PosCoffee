d419b6ae17ae63459f467b1d6e43da76ccff50c2
pondnarawich <pondnarawich@gmail.com>
2021-06-24 09:48:33 +0700

    -

--- calculation/calculation.go
+++ calculation/calculation.go
@@ -12,33 +12,39 @@ type CreateMoneyStruct struct {
 	Currency	string
 }
 
-func Calculation(value int64, note []CreateMoneyStruct) (map[int64]int64, error){
+func Calculation(paid int64, price int64, note []CreateMoneyStruct) ([]CreateMoneyStruct, map[int64]int64, error){
+	value := paid - price
 	change := make(map[int64]int64)
-	for _,i := range note{
+	for x,i := range note{
 		if value >= i.Value{
 			if value/i.Value > i.Amount{
 				change[i.Value] = i.Amount
 				value = value - (i.Amount*i.Value)
+				note[x].Amount = 0
 			} else {
 				change[i.Value] = value/i.Value
+				note[x].Amount = i.Amount - (value/i.Value)
 				value = value % i.Value
 			}
 		}
 	}
 	if value != 0{
-		return nil, fmt.Errorf("error: not enough change")
+		return note,nil, fmt.Errorf("error: not enough change")
 	}
-	return change,nil
+	return note, change,nil
 }
 
 func main(){
 	thai := []CreateMoneyStruct{{"-",1000,20,"-"},{"-",500,20,"-"},{"-",100,20,"-"},{"-",50,20,"-"},
 		{"-",20,20,"-"},{"-",10,20,"-"},{"-",5,20,"-"},{"-",2,20,"-"},{"-",1,20,"-"}}
-	change, err := Calculation(9999,thai)
+	remain,change, err := Calculation(9999,0,thai)
 	if err != nil{
 		log.Fatal(err)
 	}
 	fmt.Println(change)
+	for _,x := range remain{
+		fmt.Println("asd",x)
+	}
 }
 
 //Default
--- calculation/calculation_test.go
+++ calculation/calculation_test.go
@@ -8,7 +8,7 @@ import (
 func TestDefault(t *testing.T){
 	thai := []CreateMoneyStruct{{"-",1000,20,"-"},{"-",500,20,"-"},{"-",100,20,"-"},{"-",50,20,"-"},
 		{"-",20,20,"-"},{"-",10,20,"-"},{"-",5,20,"-"},{"-",2,20,"-"},{"-",1,20,"-"}}
-	change,_:=Calculation(9999, thai)
+	_,change,_:=Calculation(9999,0, thai)
 	expectedResult := map[int64]int64{2:2,5:1,20:2,50:1,100:4,500:1,1000:9}
 	eq := reflect.DeepEqual(change, expectedResult)
 	if eq != true {
@@ -19,7 +19,7 @@ func TestDefault(t *testing.T){
 func TestNotEnoughNote(t *testing.T) {
 	thai := []CreateMoneyStruct{{"-",1000,2,"-"},{"-",500,20,"-"},{"-",100,20,"-"},{"-",50,20,"-"},
 		{"-",20,20,"-"},{"-",10,20,"-"},{"-",5,20,"-"},{"-",2,20,"-"},{"-",1,20,"-"}}
-	change,_:=Calculation(9999, thai)
+	_,change,_:=Calculation(9999,0, thai)
 	expectedResult := map[int64]int64{2:2,5:1, 20:2, 50:1, 100:4, 500:15, 1000:2}
 	eq := reflect.DeepEqual(change, expectedResult)
 	if eq != true {
@@ -30,7 +30,7 @@ func TestNotEnoughNote(t *testing.T) {
 func TestNotEnoughChange(t *testing.T) {
 	thai := []CreateMoneyStruct{{"-",1000,2,"-"},{"-",500,20,"-"},{"-",100,20,"-"},{"-",50,20,"-"},
 		{"-",20,20,"-"},{"-",10,20,"-"},{"-",5,20,"-"},{"-",2,20,"-"},{"-",1,20,"-"}}
-	_, err:=Calculation(99999, thai)
+	_,_, err:=Calculation(99999,0, thai)
 	if err == nil {
 		t.Errorf("%v",err)
 	}
