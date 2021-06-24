b2bfdbe8f4b3bb27e26a618b43791adf75c05a86
proudkittapa <proudy2013@gmail.com>
2021-06-24 16:08:30 +0700

    grpc

--- cart/setup.go
+++ cart/setup.go
@@ -3,18 +3,25 @@ package main
 import (
 	"context"
 	"github.com/gnnchya/PosCoffee/cart/config"
+	grpcService "github.com/gnnchya/PosCoffee/cart/service/grpcClient/implement"
 	"log"
 
 	"github.com/gnnchya/PosCoffee/cart/app"
 	validatorService "github.com/gnnchya/PosCoffee/cart/service/validator"
 
+	repoGrpc "github.com/gnnchya/PosCoffee/cart/repository/grpc"
 	userRepo "github.com/gnnchya/PosCoffee/cart/repository/user"
 	userService "github.com/gnnchya/PosCoffee/cart/service/user/implement"
 )
 
+const (
+	NETWORK = "tcp"
+)
 func newApp(appConfig *config.Config) *app.App {
 	ctx := context.Background()
 	uRepo, err := userRepo.New(ctx, appConfig.MongoDBEndpoint, appConfig.MongoDBName, appConfig.MongoDBTableName)
+	grpcRepo := repoGrpc.New(configGrpc(appConfig))
+	gService := grpcService.New(grpcRepo)
 	panicIfErr(err)
 	validator := validatorService.New(uRepo)
 
@@ -27,5 +34,12 @@ func newApp(appConfig *config.Config) *app.App {
 func panicIfErr(err error) {
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func configGrpc(appConfig *config.Config) *repoGrpc.Config {
+	return &repoGrpc.Config{
+		Network: NETWORK,
+		Port:    appConfig.GRPCSenderHost,
 	}
 }
