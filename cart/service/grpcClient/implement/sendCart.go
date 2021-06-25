ca62ffa0bd634c25701c1415f9700245fcdd48f1
proudkittapa <proudy2013@gmail.com>
2021-06-25 16:42:20 +0700

    Update sendCart.go

--- cart/service/grpcClient/implement/sendCart.go
+++ cart/service/grpcClient/implement/sendCart.go
@@ -2,13 +2,13 @@ package implement
 
 import (
 	"context"
-	"github.com/gnnchya/PosCoffee/cart/service/grpc/protobuf"
+	"github.com/gnnchya/PosCoffee/cart/service/grpcClient/protobuf"
 	"time"
 )
 
 func (impl *implementation) SendCart(input *protobuf.Request) (*protobuf.Reply, error){
 	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	r, err := impl.client.SendCart(ctx, input)
+	r, err := impl.client.ReceiveChange(ctx, input)
 	if err != nil {
 		return r, err
 	}
