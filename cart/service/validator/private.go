e4a77968fb028331ac225f10fe02829c17f4ef5e
pondnarawich <pondnarawich@gmail.com>
2021-06-25 12:00:20 +0700

    -

--- cart/service/validator/private.go
+++ cart/service/validator/private.go
@@ -2,8 +2,11 @@ package validator
 
 import (
 	"fmt"
+	"github.com/gnnchya/PosCoffee/cart/domain"
+	"github.com/gnnchya/PosCoffee/cart/service/user/userin"
 	"github.com/go-playground/validator/v10"
 	"regexp"
+	"strconv"
 )
 
 func (v *GoPlayGroundValidator) checkName(structLV validator.StructLevel, name string) {
@@ -22,20 +25,19 @@ func (v *GoPlayGroundValidator) checkTH(structLV validator.StructLevel, name str
 }
 
 
-//func (v *GoPlayGroundValidator) checkAmountStruct(structLV validator.StructLevel, menu userin.Input){
-//	if menu.Menu < 0{
-//		structLV.ReportError(menu.Amount, "err validation for menu", "err validation for menu", "zero", "")
-//	}
-//
-//}
+func (v *GoPlayGroundValidator) checkAmountStruct(structLV validator.StructLevel, menu userin.Input){
+	if len(menu.Menu) < 0{
+		structLV.ReportError(len(menu.Menu), "err validation for menu", "err validation for menu", "zero", "")
+	}
+}
 
-//func (v *GoPlayGroundValidator) checkUpdateStruct(structLV validator.StructLevel, cart []domain.Cart){
-//	if len(cart) == 0{
-//		structLV.ReportError(cart, "err validation cart is 0", "err validation cart is 0", "zero", "")
-//	}
-//	for i, j := range cart{
-//		if j.Amount <= 0{
-//			structLV.ReportError(cart, "err validation for cart #"+strconv.Itoa(i), "err validation for cart #"+strconv.Itoa(i), "zero", "")
-//		}
-//	}
-//}
+func (v *GoPlayGroundValidator) checkUpdateStruct(structLV validator.StructLevel, cart domain.CreateStruct){
+	if len(cart.Menu) == 0{
+		structLV.ReportError(cart, "err validation cart is 0", "err validation cart is 0", "zero", "")
+	}
+	for i, j := range cart.Menu{
+		if j.Amount <= 0{
+			structLV.ReportError(cart, "err validation for cart #"+strconv.Itoa(i), "err validation for cart #"+strconv.Itoa(i), "zero", "")
+		}
+	}
+}
--- cart/service/validator/updateUser.go
+++ cart/service/validator/updateUser.go
@@ -1,9 +1,14 @@
 package validator
 
-//func (v *GoPlayGroundValidator) UserUpdateStructLevelValidation(structLV validator.StructLevel) {
-//	input := structLV.Current().Interface().(userin.CreateMenu)
-//	v.checkAmountStruct(structLV, input)
-//	//v.checkUserIDUnique(ctx, structLV, input.ID)
-//	// v.checkName(structLV, input.Name)
-//}
+import (
+	"github.com/gnnchya/PosCoffee/cart/service/user/userin"
+	"github.com/go-playground/validator/v10"
+)
+
+func (v *GoPlayGroundValidator) UserUpdateStructLevelValidation(structLV validator.StructLevel) {
+	input := structLV.Current().Interface().(userin.Input)
+	v.checkAmountStruct(structLV, input)
+	//v.checkUserIDUnique(ctx, structLV, input.ID)
+	// v.checkName(structLV, input.Name)
+}
 
