b9865308c1b9968295e6b4b14fb6dbef20796c40
pondnarawich <pondnarawich@gmail.com>
2021-06-25 11:52:36 +0700

    -

--- cart/domain/query.go
+++ cart/domain/query.go
@@ -1,26 +1,26 @@
 package domain
 
 type Ingredient struct{
-	name      string   `bson:"item_name"`
-	Amount     int64    `bson:"amount"`
+	ItemName      string   `bson:"item_name" json:"item_name"`
+	Amount     int64    `bson:"amount" json:"amount"`
 }
 
 type Menu struct{
-	ID         		string   `bson:"_id"`
-	Category       	[]string   `bson:"category"`
-	Name 			string   `bson:"name"`
-	Ingredient 		[]Ingredient `bson:"ingredient"`
-	Price      		int64    `bson:"price"`
-	Available 		bool	 `bson:"available"`
-	Amount 			int64    `bson:"amount"`
-	Option 			string   `bson:"option"`
+	ID         		string   `bson:"_id" json:"_id"`
+	Category       	[]string   `bson:"category" json:"category"`
+	Name 			string   `bson:"name" json:"name"`
+	Ingredient 		[]Ingredient `bson:"ingredient" json:"ingredient"`
+	Price      		int64    `bson:"price" json:"price"`
+	Available 		bool	 `bson:"available" json:"available"`
+	Amount 			int64    `bson:"amount" json:"amount"`
+	Option 			string   `bson:"option" json:"option"`
 }
 
 type CreateStruct struct {
-	ID 				string  `bson:"_id"`
-	CustomerID 		string  `bson:"customer_id"`
-	Menu			[]Menu	`bson:"menu"`
-	TotalPrice		int64	`bson:"total_price"`
+	ID 				string  `bson:"_id" json:"_id"`
+	CustomerID 		string  `bson:"customer_id" json:"customer_id"`
+	Menu			[]Menu	`bson:"menu" json:"menu"`
+	TotalPrice		int64	`bson:"total_price" json:"total_price"`
 }
 
 type DeleteStruct struct {
@@ -33,7 +33,7 @@ type UpdateStruct struct {
 	ID 				string  `bson:"_id" json:"_id"`
 	CustomerID 		string  `bson:"customer_id" json:"customer_id"`
 	Menu			[]Menu 	`bson:"menu" json:"menu"`
-	TotalPrice		int64	`bson:"total_price"`
+	TotalPrice		int64	`bson:"total_price" json:"total_price"`
 }
 
 type ReadStruct struct {
