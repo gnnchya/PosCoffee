9616e501a4fa572f848436d12550f6b10fbba010
pondnarawich <pondnarawich@gmail.com>
2021-06-28 09:35:52 +0700

    -

--- product/service/report/report.go
+++ product/service/report/report.go
@@ -1,1 +1,21 @@
 package report
+
+import "github.com/gnnchya/PosCoffee/product/domain"
+
+func Report(transaction []domain.CreateOrderStruct, outOfStock []domain.CreateStockStruct ,usedStock []domain.CreateStockStruct) (result ReportStruct){
+	result.Transaction = transaction
+	result.UsedStock = usedStock
+	result.OutOfStock = outOfStock
+	result.TotalIncome, result.TotalCost, result.TotalProfit = CalculateProfit(transaction)
+	return result
+}
+
+func CalculateProfit(transaction []CreateOrderStruct) (int64, int64, int64){
+	var TotalIncome int64
+	var TotalCost int64
+	for _,order := range transaction{
+		TotalIncome = TotalIncome + order.Cart.TotalPrice
+		TotalCost = TotalCost + order.TotalCost
+	}
+	return TotalIncome, TotalCost, TotalIncome-TotalCost
+}
