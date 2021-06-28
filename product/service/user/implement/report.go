7d276d3f55ed7bf7e34a2f65bc441e5e581f285d
proudkittapa <proudy2013@gmail.com>
2021-06-28 16:21:54 +0700

    Update report.go

--- product/service/user/implement/report.go
+++ product/service/user/implement/report.go
@@ -2,6 +2,7 @@ package implement
 
 import (
 	"context"
+	"github.com/gnnchya/PosCoffee/product/domain"
 	"github.com/gnnchya/PosCoffee/product/service/report"
 	"github.com/gnnchya/PosCoffee/product/service/user/userin"
 	"os"
@@ -10,6 +11,7 @@ import (
 func(impl *implementation)Report(ctx context.Context, input *userin.ReportRange) *os.File{
 	transaction,_ := impl.repo.ReadByTimeRange(ctx, input.From,input.Until)
 	//stock := //proud
+	var stock []domain.CreateStockStruct
 	report := report.Report(transaction, stock)
 	return report
 }
