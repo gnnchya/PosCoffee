1439ecb585fc0beb1b13951d35e2d144b4e4b14b
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-06-28 14:24:50 +0700

    Update create.go

--- product/service/user/implement/create.go
+++ product/service/user/implement/create.go
@@ -55,7 +55,7 @@ func (impl *implementation) Create(ctx context.Context, input *userin.CreateInpu
 	return change, nil
 }
 
-func Find(slice []string, val string) (bool) {
+func Find(slice []string, val string) bool{
 	for _, item := range slice {
 		if item == val {
 			return  true
