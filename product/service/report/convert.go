d6df43e2b5fe4c6e9ae44bcfe3c49d7e407fa524
pondnarawich <pondnarawich@gmail.com>
2021-06-29 16:01:04 +0700

    -

--- product/service/report/convert.go
+++ product/service/report/convert.go
@@ -23,6 +23,7 @@ func TranToArray(transaction []domain.CreateOrderStruct) (res [][]string){
 	for _,i := range transaction{
 		temp = []string{}
 		for _,x := range i.Cart.Menu{
+			temp = []string{}
 			if contains(res,i.ID){
 				for q:=0; q < 10; q++{
 					temp = append(temp, " ")
