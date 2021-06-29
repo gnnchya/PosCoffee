0a598b52920f717e7c90e80a799b1539b6d9914a
proudkittapa <proudy2013@gmail.com>
2021-06-29 11:47:00 +0700

    finish api cart

--- cart/app/user/finish.go
+++ cart/app/user/finish.go
@@ -3,11 +3,18 @@ package user
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gnnchya/PosCoffee/cart/app/view"
+	"github.com/gnnchya/PosCoffee/cart/service/user/userin"
 )
 
 func (ctrl *Controller) Finish(c *gin.Context) {
+	//TODO input from user
+	cart := &userin.FinishInput{}
+	if err := c.ShouldBindJSON(cart); err != nil {
+		view.MakeErrResp2(c, 422, err)
+		return
+	}
 	id := c.Param("id")
-	a, err := ctrl.service.Finish(c, id)
+	a, err := ctrl.service.Finish(c, id, cart)
 
 	if err != nil {
 		view.MakeErrResp2(c, 422, err)
