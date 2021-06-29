0aa1f2345f76ba2a2ac17cbb1795c9089d7297ee
proudkittapa <proudy2013@gmail.com>
2021-06-29 10:36:49 +0700

    -

---
+++ stock/service/grpc/implement/sendReport.go
@@ -0,0 +1,35 @@
+package implement
+
+import (
+	"context"
+	"fmt"
+	pb "github.com/gnnchya/PosCoffee/stock/service/grpc/protobuf/report"
+)
+
+func(impl implementation) SendReport(ctx context.Context, input *pb.ReportRequest) (*pb.ReportReply, error){
+	fmt.Println("from product ask for report", input)
+	report, err := impl.userService.Report(ctx)
+	if err != nil {
+		return nil, err
+	}
+	var result []*pb.ReportReplyStruct
+	for _, v := range report{
+		temp := &pb.ReportReplyStruct{
+			ID:          v.ID,
+			ItemName:    v.ItemName,
+			Category:    v.Category,
+			Amount:      v.Amount,
+			Unit:        v.Unit,
+			CostPerUnit: v.CostPerUnit,
+			EXPDate:     v.EXPDate,
+			ImportDate:  v.ImportDate,
+			Supplier:    v.Supplier,
+			TotalCost:   v.TotalCost,
+			TotalAmount: v.TotalAmount,
+			Status:      v.Status,
+		}
+		result = append(result, temp)
+	}
+	out := &pb.ReportReply{Report: result}
+	return out, nil
+}
--- stock/service/user/implement/report.go
+++ stock/service/user/implement/report.go
@@ -4,10 +4,9 @@ import (
 	"context"
 	"fmt"
 	"github.com/gnnchya/PosCoffee/stock/domain"
-	"github.com/gnnchya/PosCoffee/stock/service/user/userin"
 )
 
-func (impl *implementation) Report(ctx context.Context, input *userin.CreateInput) (result []domain.CreateStruct, err error) {
+func (impl *implementation) Report(ctx context.Context) (result []domain.CreateStruct, err error) {
 	result, err = impl.repo.Report(ctx)
 	if err != nil {
 		fmt.Println("err in report", err)
