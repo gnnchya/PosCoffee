e1b0e8684a1ef5f489e36c7098cb20b949bcd045
pondnarawich <pondnarawich@gmail.com>
2021-06-29 17:36:25 +0700

    -

--- menu/repository/elastic/convert.go
+++ menu/repository/elastic/convert.go
@@ -11,13 +11,7 @@ func InToStruct(r map[string]interface{}) []domain.CreateStruct{
 	var result []domain.CreateStruct
 	for _, hit := range r["hits"].(map[string]interface{})["hits"].([]interface{}) {
 		s := hit.(map[string]interface{})["_source"]
-		temp.ID = fmt.Sprintf("%v", s.(map[string]interface{})["id"])
-		if fmt.Sprintf("%v", s.(map[string]interface{})["id"]) == ""{
-			temp.ID = fmt.Sprintf("%v", s.(map[string]interface{})["_id"])
-			fmt.Println("if------------------------ id ", temp.ID)
-		}
-		fmt.Println("id------------------",temp.ID)
-		temp.ID = fmt.Sprintf("%v", s.(map[string]interface{})["id"])
+		temp.ID = fmt.Sprintf("%v", hit.(map[string]interface{})["id"])
 		temp.Name = fmt.Sprintf("%v", s.(map[string]interface{})["name"])
 		temp.Category= strings.Split(fmt.Sprintf("%v", s.(map[string]interface{})["category"]),",")
 		temp.Ingredient = make([]domain.Ingredient, len(s.(map[string]interface{})["ingredient"].([]interface{})))
