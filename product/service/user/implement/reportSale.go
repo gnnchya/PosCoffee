3cad28223da9a559635d4a8d702ab57de34fc891
proudkittapa <proudy2013@gmail.com>
2021-06-30 17:50:49 +0700

    Update reportSale.go

--- product/service/user/implement/reportSale.go
+++ product/service/user/implement/reportSale.go
@@ -2,6 +2,8 @@ package implement
 
 import (
 	"context"
+	"github.com/gnnchya/PosCoffee/product/domain"
+	"github.com/gnnchya/PosCoffee/product/service/grpcClient/protobuf"
 	"github.com/gnnchya/PosCoffee/product/service/reportSale"
 	"github.com/gnnchya/PosCoffee/product/service/user/userin"
 )
@@ -9,6 +11,29 @@ import (
 func(impl *implementation)ReportSale(ctx context.Context, input *userin.ReportRange) ([][]string, error){
 	total, err := impl.repo.ReadMenuTotalSale(ctx,input.From,input.Until)
 	//menu := //proud
+	request := &protobuf.RequestMenu{Err: "nil"}
+	reply, err := impl.client.SendMenu(request)
+	var menu []domain.OldMenu
+	for _, k := range reply.Menu{
+		var ingre []domain.Ingredient
+
+		for _, j := range k.Ingredient{
+			in := domain.Ingredient{
+				IngredientName: j.IngredientName,
+				Amount:         j.Amount,
+			}
+			ingre = append(ingre, in)
+		}
+		m := domain.OldMenu{
+			ID:         k.ID,
+			Category:   k.Category,
+			Name:       k.Name,
+			Ingredient: ingre,
+			Price:      k.Price,
+			Available:  k.Available,
+		}
+		menu = append(menu, m)
+	}
 	rangeReport := reportSale.ReportSale(total,menu)
 	return rangeReport, err
 }
