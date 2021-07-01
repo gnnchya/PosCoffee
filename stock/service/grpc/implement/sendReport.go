92ba43d7a50af4e8a865fa1d4120dff4c5d364eb
proudkittapa <proudy2013@gmail.com>
2021-07-01 10:05:26 +0700

    Update sendReport.go

--- stock/service/grpc/implement/sendReport.go
+++ stock/service/grpc/implement/sendReport.go
@@ -3,12 +3,20 @@ package implement
 import (
 	"context"
 	"fmt"
+	"github.com/gnnchya/PosCoffee/stock/domain"
 	pb "github.com/gnnchya/PosCoffee/stock/service/grpc/protobuf/report"
 )
 
 func(impl implementation) SendReportToStock(ctx context.Context, input *pb.ReportRequest) (*pb.ReportReply, error){
 	fmt.Println("from product ask for report", input)
-	report, err := impl.userService.Report(ctx)
+	var err error
+	var report []domain.CreateStruct
+	if input.Request == "stock"{
+		report, err = impl.userService.Report(ctx)
+	}else if input.Request == "reportSale"{
+		report, err = impl.userService.ReportStock(ctx, input.Field, input.Order)
+	}
+	//report, err := impl.userService.Report(ctx)
 	fmt.Println("report", report)
 	if err != nil {
 		return nil, err
