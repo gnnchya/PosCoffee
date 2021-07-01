480c3f273218a24667c4d7dbaff2bd0b8754d96d
proudkittapa <proudy2013@gmail.com>
2021-07-01 16:05:36 +0700

    -

--- cart/service/bill/buildPdf.go
+++ cart/service/bill/buildPdf.go
@@ -8,15 +8,15 @@ func GeneratePdf(filename string, order []string) (*gofpdf.Fpdf,error) {
 	pdf := gofpdf.New("P", "mm", "A6", "")
 	pdf.AddPage()
 	pdf.SetFont("Arial", "B", 11)
-	pdf.ImageOptions(
-		"Untitled_Artwork.jpg",
-		35, 10,
-		30, 30,
-		false,
-		gofpdf.ImageOptions{ImageType: "JPG", ReadDpi: true},
-		0,
-		"",
-	)
+	//pdf.ImageOptions(
+	//	"Untitled_Artwork.jpg",
+	//	35, 10,
+	//	30, 30,
+	//	false,
+	//	gofpdf.ImageOptions{ImageType: "JPG", ReadDpi: true},
+	//	0,
+	//	"",
+	//)
 	for _,i := range order{
 		pdf.CellFormat(0, 70, i, "0", 0, "CM", false, 0, "")
 	}
