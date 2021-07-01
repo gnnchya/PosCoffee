c82478804f2f29147acfd8b90d8e9cd5bae62a3c
proudkittapa <proudy2013@gmail.com>
2021-07-01 14:21:43 +0700

    done grpc read

--- product/app/init.go
+++ product/app/init.go
@@ -34,8 +34,8 @@ func (app *App) RegisterRoute(router *gin.Engine) *App {
 		apiRoutes.DELETE("/product/stock/:id", app.user.DeleteStock)
 		apiRoutes.GET("/product/report", app.user.Report)
 		apiRoutes.GET("/product/stock/:id", app.user.ReadStock)
-		apiRoutes.GET("/product/stock/name", app.user.ReadNameStock)
-		apiRoutes.GET("/product/stock/category", app.user.ReadCategoryStock)
+		apiRoutes.GET("/product/stock/name/:name", app.user.ReadNameStock)
+		apiRoutes.GET("/product/stock/category/:category", app.user.ReadCategoryStock)
 		apiRoutes.GET("/product/reportSale", app.user.ReportSale)
 		apiRoutes.GET("/product/reportStock", app.user.ReportStock)
 	}
--- product/app/user/readNameStock.go
+++ product/app/user/readNameStock.go
@@ -4,13 +4,30 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gnnchya/PosCoffee/product/app/view"
 	"github.com/gnnchya/PosCoffee/product/service/user/userin"
+	"strconv"
 )
 
 func (ctrl *Controller) ReadNameStock(c *gin.Context) {
+	name := c.Param("name")
 	input := &userin.ReadNameAllInput{}
+	limit := 2
+	page := 0
+	query := c.Request.URL.Query()
+	for key, value := range query {
+		queryValue := value[len(value)-1]
+		switch key {
+		case "limit":
+			limit, _ = strconv.Atoi(queryValue)
+		case "page":
+			page, _ = strconv.Atoi(queryValue)
+		}
+	}
+	input.Page = page
+	input.PerPage = limit
+	input.ItemName = name
 	a, err := ctrl.service.ReadNameStock(c, input)
 	if err != nil {
-		view.MakeErrResp2(c, 400, err)
+		view.MakeErrResp(c, 422, "error read name")
 		return
 	}
 
