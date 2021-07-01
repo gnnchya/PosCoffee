a35dc47783011f0a0d35f64df94cd54040b5d56d
proudkittapa <proudy2013@gmail.com>
2021-07-01 15:49:18 +0700

    Update finish.go

--- cart/app/user/finish.go
+++ cart/app/user/finish.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gnnchya/PosCoffee/cart/app/view"
 	"github.com/gnnchya/PosCoffee/cart/service/bill"
@@ -15,18 +16,19 @@ func (ctrl *Controller) Finish(c *gin.Context) {
 		return
 	}
 	id := c.Param("id")
-	a, order, err := ctrl.service.Finish(c, id, cart)
+	_, order, err := ctrl.service.Finish(c, id, cart)
 	var filename = "bill.pdf"
-	var _, _ = bill.GeneratePdf(filename, order)
+	b, err := bill.GeneratePdf(filename, order)
+	fmt.Println("error bill", err)
 	if err != nil {
-		view.MakeErrResp(c, 422, "error report")
+		view.MakeErrResp(c, 422, "error finish")
 		return
 	}
 	c.File(filename)
-	if err != nil {
-		view.MakeErrResp2(c, 422, err)
-		return
-	}
+	//if err != nil {
+	//	view.MakeErrResp2(c, 422, err)
+	//	return
+	//}
 
-	view.MakeSuccessResp(c, 200, a)
+	view.MakeSuccessResp(c, 200, b)
 }
