56b6ce569f59f03e43a5d5589b671cade7d6762b
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-07-05 10:04:09 +0700

    clean the code

--- cart/app/user/create.go
+++ cart/app/user/create.go
@@ -1,7 +1,6 @@
 package user
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gnnchya/PosCoffee/cart/app/view"
 	"github.com/gnnchya/PosCoffee/cart/service/user/userin"
@@ -13,12 +12,7 @@ func (ctrl *Controller) Create(c *gin.Context) {
 		view.MakeErrResp2(c, 422, err)
 		return
 	}
-	fmt.Println("input create app:", cart)
-	//input := &userin.CreateInput{}
-	//initID := goxid.New()
-	//cart.ID = initID.Gen()
-	//initID = goxid.New()
-	//cart.CustomerID = initID.Gen()
+
 	_, err := ctrl.service.Create(c, cart)
 	if err != nil {
 		view.MakeErrResp2(c, 422, err)
