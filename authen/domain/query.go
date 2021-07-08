b1aa52de5e65a621583581a13713ffc9a0185420
pondnarawich <pondnarawich@gmail.com>
2021-07-08 11:03:44 +0700

    -

--- authen/domain/query.go
+++ authen/domain/query.go
@@ -34,16 +34,19 @@ type ConsumerStruct struct {
 	ID 				string 	`bson:"_id" json:"id"`
 	ClientID		string 	`bson:"client_id" json:"client_id"`
 	ClientSecret	string 	`bson:"client_secret" json:"client_secret"`
+	Scope           string 	`bson:"scope" json:"scope"`
+	CreatedAt       int64  	`bson:"createdAt" json:"createdAt"`
 }
 
 type TokenStruct struct {
 	ID 				string 	`bson:"_id" json:"id"`
 	UID 			string	`bson:"uid" json:"uid"`
-	Token 			string	`bson:"token" json:"token"`
-	Expire 			string	`bson:"expire" json:"expire"`
+	AccessToken 	string	`bson:"access_token" json:"access_token"`
+	Expire 			int64	`bson:"expire" json:"expire"`
 	RefreshToken	string	`bson:"refresh_token" json:"refresh_token"`
-	RefreshExpire	string	`bson:"refresh_expire" json:"refresh_expire"`
+	RefreshExpire	int64	`bson:"refresh_expire" json:"refresh_expire"`
 }
+
 
 type PermissionStruct struct{
 	ID			string
