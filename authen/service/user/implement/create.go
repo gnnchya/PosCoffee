e78955212725c255deb61d55593399fdb91b6341
pondnarawich <pondnarawich@gmail.com>
2021-07-08 10:01:50 +0700

    -

--- authen/service/user/implement/create.go
+++ authen/service/user/implement/create.go
@@ -57,22 +57,22 @@ func (impl *implementation) Create(ctx context.Context, input *userin.CreateInpu
 	return user.ID, nil
 }
 
-func (impl *implementation) sendMsgCreate(input *usersin.CreateInput) (err error) {
-	return impl.MsgSender(msgbrokerin.TopicOTP, usersin.MsgBrokerCreate{
-		Action:         msgbrokerin.ActionCreate,
-		ID:             input.ID,
-		Prefix:         input.Prefix,
-		FirstName:      input.FirstName,
-		LastName:       input.LastName,
-		SignUpChannel:  input.SignUpChannel,
-		Email:          input.Email,
-		MobileNumber:   input.MobileNumber,
-		IdentifyType:   input.IdentifyType,
-		IdentifyNumber: input.IdentifyNumber,
-		Password:       input.Password,
-		Passcode:       input.PassCode,
-		MetaData:       input.MetaData,
-		RoleID:         input.RoleID,
-		MemberGroup:    input.MemberGroup,
-	})
-}
+//func (impl *implementation) sendMsgCreate(input *usersin.CreateInput) (err error) {
+//	return impl.MsgSender(msgbrokerin.TopicOTP, usersin.MsgBrokerCreate{
+//		Action:         msgbrokerin.ActionCreate,
+//		ID:             input.ID,
+//		Prefix:         input.Prefix,
+//		FirstName:      input.FirstName,
+//		LastName:       input.LastName,
+//		SignUpChannel:  input.SignUpChannel,
+//		Email:          input.Email,
+//		MobileNumber:   input.MobileNumber,
+//		IdentifyType:   input.IdentifyType,
+//		IdentifyNumber: input.IdentifyNumber,
+//		Password:       input.Password,
+//		Passcode:       input.PassCode,
+//		MetaData:       input.MetaData,
+//		RoleID:         input.RoleID,
+//		MemberGroup:    input.MemberGroup,
+//	})
+//}
--- authen/service/util/repository.go
+++ authen/service/util/repository.go
@@ -3,6 +3,7 @@ package util
 import (
 	"context"
 	"github.com/gnnchya/PosCoffee/authen/service/msgbroker/msgbrokerin"
+	"github.com/gnnchya/PosCoffee/authen/service/user/userin"
 	"github.com/touchtechnologies-product/message-broker/common"
 	"google.golang.org/grpc"
 )
@@ -10,15 +11,7 @@ import (
 //go:generate mockery --name=Repository
 
 type Repository interface {
-	Create(ctx context.Context, ent interface{}) (err error)
-	Update(ctx context.Context, ent interface{}, ID string) (err error)
-	Delete(ctx context.Context, id string) (err error)
-	//SearchName(ctx context.Context, s *domain.SearchValue) (result []domain.Users, err error)
-	//Read(ctx context.Context, id string) (a domain.Users, err error)
-	//ReadAll(ctx context.Context, perPage int, page int) ([]domain.Users, error)
-	CheckExistID(ctx context.Context, id string) (bool, error)
-	CheckExistCustomerID(ctx context.Context, id string) (bool, error)
-	CheckExistInCart(ctx context.Context, id string) (bool, error)
+	Create(ctx context.Context, input userin.CreateInput) (err error)
 }
 
 type RepositoryUsers interface {
