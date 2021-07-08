9eca41060446ea79cb2e1549570dc6c4fffbb86d
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-07-08 23:45:22 +0700

    Update private.go

--- oAuth/service/validator/private.go
+++ oAuth/service/validator/private.go
@@ -3,22 +3,7 @@ package validator
 import (
 	"context"
 	"github.com/go-playground/validator/v10"
-	"regexp"
 )
-
-func (v *GoPlayGroundValidator) checkName(structLV validator.StructLevel, name string) {
-	if name != "Proud" {
-		structLV.ReportError("not Proud", "name", "name", "unique", "")
-	}
-}
-
-func (v *GoPlayGroundValidator) checkTH(structLV validator.StructLevel, name string) {
-	re := regexp.MustCompile("[A-Za-z]+")
-	ok := re.MatchString(name)
-	if !ok {
-		structLV.ReportError(name, "err validation", "err validation", "match", "")
-	}
-}
 
 func (v *GoPlayGroundValidator) checkConsumerExistID(structLV validator.StructLevel, ID string) {
 	exist, _ := v.consumerRepo.CheckExistID(context.Background(), ID)
