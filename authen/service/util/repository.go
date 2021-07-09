817ee8cf835ba7fd12b1c704298a4557957e08fb
pondnarawich <pondnarawich@gmail.com>
2021-07-09 10:43:56 +0700

    -

--- authen/service/util/repository.go
+++ authen/service/util/repository.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"context"
+	"github.com/gnnchya/PosCoffee/authen/domain"
 )
 
 //go:generate mockery --name=Repository
@@ -10,6 +11,7 @@ type Repository interface {
 	Create(ctx context.Context, ent interface{}) (ID string, err error)
 	Read(ctx context.Context, filters []string, out interface{}) (err error)
 	Update(ctx context.Context, filters []string, ret interface{}) (err error)
+	List(ctx context.Context, opt *domain.PageOption, itemType interface{}) (total int, items []interface{}, err error)
 }
 
 type RepositoryUsers interface {
