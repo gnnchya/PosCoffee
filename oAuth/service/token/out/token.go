849844c5b5d66b49c28a4021244619d225a2d078
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-07-09 10:30:09 +0700

    Update token.go

--- oAuth/service/token/out/token.go
+++ oAuth/service/token/out/token.go
@@ -5,13 +5,13 @@ import "github.com/gnnchya/PosCoffee/oAuth/domain"
 type TokenView struct {
 	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
-	AccessExpire int64  `json:"expired"`
+	Expired      int64  `json:"expired"`
 }
 
 func TokenToView(token *domain.TokenStruct) (view *TokenView) {
 	return &TokenView{
 		AccessToken:  token.AccessToken,
 		RefreshToken: token.RefreshToken,
-		AccessExpire: token.AccessExpire,
+		Expired:      token.AccessExpire,
 	}
 }
