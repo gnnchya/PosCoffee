02c64dc54fae6532168acf4e5659cfed259e0d0a
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-07-13 10:37:50 +0700

    -

--- authen/service/util/repository.go
+++ authen/service/util/repository.go
@@ -12,6 +12,7 @@ type Repository interface {
 	Create(ctx context.Context, ent interface{}) (ID string, err error)
 	Read(ctx context.Context, filters []string, out interface{}) (err error)
 	Update(ctx context.Context, filters []string, ret interface{}) (err error)
+	Count(ctx context.Context, filters []string) (total int, err error)
 	List(ctx context.Context, opt *domain.PageOption, itemType interface{}) (total int, items []interface{}, err error)
 }
 
--- authen/service/validator/private.go
+++ authen/service/validator/private.go
@@ -25,7 +25,13 @@ func (v *GoPlayGroundValidator) checkTH(structLV validator.StructLevel, name str
 }
 
 func (v *GoPlayGroundValidator) checkUsername(structLV validator.StructLevel, name string) {
-	//TODO check if this username exist
+	filters := []string{fmt.Sprintf("username:eq:%s", name)}
+	if found , err := v.repo.Count(context.Background(), filters); err != nil{
+		//TODO report error
+		//structLV.ReportError(mobileNumber, "mobileNumber", "mobileNumber", "unique", "")
+	} else if found >0{
+		structLV.ReportError(name, "username", "username", "unique", "")
+	}
 	re := regexp.MustCompile("^[a-zA-Z][a-zA-Z0-9]{7,19}$")
 	ok := re.MatchString(name)
 	if !ok {
@@ -117,11 +123,13 @@ func (v *GoPlayGroundValidator) checkBankAccount(structLV validator.StructLevel,
 }
 
 func (v *GoPlayGroundValidator) checkMobileNumberUnique(ctx context.Context, structLV validator.StructLevel, mobileNumber string) {
-	//filters := []string{fmt.Sprintf("mobileNumber:eq:%s", mobileNumber), deleteAt}
-	//if err := v.usersRepo.Read(ctx, filters, &domain.Users{}); err == nil {
-	//	structLV.ReportError(mobileNumber, "mobileNumber", "mobileNumber", "unique", "")
-	//}
-	//TODO check for unique
+	filters := []string{fmt.Sprintf("mobile_number:eq:%s", mobileNumber)}
+	if found , err := v.repo.Count(context.Background(), filters); err != nil{
+		//TODO report error
+		//structLV.ReportError(mobileNumber, "mobileNumber", "mobileNumber", "unique", "")
+	} else if found >0{
+		structLV.ReportError(mobileNumber, "mobile_number", "mobile_number", "unique", "")
+	}
 	re := regexp.MustCompile("^[0-9]*$")
 	ok := re.MatchString(mobileNumber)
 	if !ok {
@@ -130,6 +138,14 @@ func (v *GoPlayGroundValidator) checkMobileNumberUnique(ctx context.Context, str
 }
 
 func (v *GoPlayGroundValidator) checkFormatEmail(structLV validator.StructLevel, email string) {
+	filters := []string{fmt.Sprintf("email:eq:%s", email)}
+	if found , err := v.repo.Count(context.Background(), filters); err != nil{
+		//TODO report error
+		//structLV.ReportError(mobileNumber, "mobileNumber", "mobileNumber", "unique", "")
+	} else if found >0{
+		structLV.ReportError(email, "email", "email", "unique", "")
+	}
+	
 	re := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 	ok := re.MatchString(email)
 	if !ok {
