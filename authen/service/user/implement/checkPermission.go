6a34cfe160677207befdd49b7d9a0a37356aaf10
proudkittapa <proudy2013@gmail.com>
2021-07-13 10:38:48 +0700

    Update checkPermission.go

--- authen/service/user/implement/checkPermission.go
+++ authen/service/user/implement/checkPermission.go
@@ -1,3 +1,18 @@
 package implement
 
+import (
+	"context"
+	"github.com/gnnchya/PosCoffee/authen/service/grpcClient/protobuf"
+)
 
+func (impl *implementation) CheckPermission(ctx context.Context, roles []string, method string, path string)(bool, error){
+	request := &protobuf.RequestPermission{
+		Roles:      roles,
+		Permission: method+path,
+	}
+	out, err := impl.client.CheckPermission(request)
+	if err != nil{
+		return false, err
+	}
+	return out.Allow, nil
+}
