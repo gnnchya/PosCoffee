608ac1330e10624621dcc98811c44074ccd2f527
pondnarawich <pondnarawich@gmail.com>
2021-07-14 17:07:03 +0700

    -

--- authen/service/user/implement/changePassword.go
+++ authen/service/user/implement/changePassword.go
@@ -1,15 +1,22 @@
 package implement
 
-import "context"
+import (
+	"context"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type changePassword struct {
 	Password 		string				`bson:"password" json:"password"`
 }
 
 func (impl *implementation)ChangePassword(ctx context.Context, uid string, password string) error{
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
 	filter := impl.filter.MakeUIDFilters(uid)
-	input := &changePassword{Password: password}
-	err := impl.repo.Update(ctx, filter, input)
+	input := &changePassword{Password: string(hashPassword)}
+	err = impl.repo.Update(ctx, filter, input)
 	if err != nil{
 		return err
 	}
