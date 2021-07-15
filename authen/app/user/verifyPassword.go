2910e46d9cb88e70ff117cf9956dff7ca056cd57
pondnarawich <pondnarawich@gmail.com>
2021-07-15 10:41:01 +0700

    -

--- authen/app/user/verifyPassword.go
+++ authen/app/user/verifyPassword.go
@@ -21,14 +21,5 @@ func (ctrl *Controller)VerifyPassword(c *gin.Context){
 	input := &userin.ReadInput{
 		ID: id,
 	}
-	user, err := ctrl.service.Read(c, input)
-	if err != nil{
-		return
-	}
-	token, err := ctrl.authService.GetToken(user.UID,user.Username,user.Password)
-	if err != nil {
-		view.MakeErrResp2(c,1, err)
-		return
-	}
-	view.MakeSuccessResp(c, http.StatusOK, token)
+	view.MakeSuccessResp(c, 200, "password verified")
 }
