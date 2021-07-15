04f0ae1d3781b8de61549e677b3192772a048321
pondnarawich <pondnarawich@gmail.com>
2021-07-15 11:36:23 +0700

    -

--- authen/service/user/implement/forgetPassword.go
+++ authen/service/user/implement/forgetPassword.go
@@ -10,8 +10,9 @@ func (impl *implementation)ForgetPassword(Email string, Token string) error{
 		return email.SendVerifyPassword(Email, Token)
 }
 
-func (impl *implementation)InputForgetPasswordToken(ctx context.Context,Email string) (out domain.UserStruct, err error){
+func (impl *implementation)InputForgetPasswordToken(ctx context.Context,Email string) (out *domain.UserStruct, err error){
+	user := &domain.UserStruct{}
 	filter := impl.filter.MakeFilterEmailString(Email)
-	err = impl.repo.Read(ctx,[]string{filter},out)
-	return out,err
+	err = impl.repo.Read(ctx,[]string{filter},user)
+	return user,err
 }
