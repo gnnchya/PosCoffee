fe4b01809fab04f846d33ace9c51f4db88209520
proudkittapa <proudy2013@gmail.com>
2021-07-16 16:23:33 +0700

    Update delete.go

--- menu/service/user/implement/delete.go
+++ menu/service/user/implement/delete.go
@@ -2,18 +2,13 @@ package implement
 
 import (
 	"context"
-	"github.com/gnnchya/PosCoffee/menu/domain"
 	"github.com/gnnchya/PosCoffee/menu/service/user/userin"
 )
 
 func (impl *implementation) Delete(ctx context.Context, input *userin.DeleteInput) (ID string, err error) {
 	user := userin.DeleteInputToUserDomain(input)
 	_ = impl.redisRepo.Del(ctx, user.ID)
-	var a domain.CreateStruct
-	err = impl.redisRepo.Get(ctx, user.ID, &a)
-	if err != nil {
-		return "", err
-	}
+
 	err = impl.elasRepo.Delete(ctx, user.ID)
 	if err != nil {
 		return "", err
